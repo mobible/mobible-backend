cc4d2ab79c02efa3aa32f2403f79be3486dd47fb
Simon de Haan <simon@praekeltfoundation.org>
2013-06-13 21:35:17 +0200

    fiddling with the admin layout

--- dbs/admin.py
+++ dbs/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from dbs.models import Journey, Verse
 
 
-admin.site.register(Journey)
-admin.site.register(Verse)
+class VerseInline(admin.StackedInline):
+    model = Verse
+    extra = 1
+
+
+class JourneyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    list_filter = ('created_at', 'updated_at')
+    read_only_fields = ('created_at', 'updated_at')
+
+    inlines = [
+        VerseInline,
+    ]
+
+admin.site.register(Journey, JourneyAdmin)
+# admin.site.register(Verse)
--- dbs/models.py
+++ dbs/models.py
@@ -12,9 +12,13 @@ class Journey(models.Model):
 
 class Verse(models.Model):
     journey = models.ForeignKey(Journey)
-    index = models.IntegerField()
-    context = models.TextField()
-    verse = models.TextField()
+    index = models.PositiveIntegerField(
+        help_text='The position of this verse in relation to other '
+                  'verses for this Journey')
+    context = models.TextField(
+        help_text='The context for this verse that is sent via SMS')
+    verse = models.TextField(
+        help_text='The verse text sent via SMS.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
