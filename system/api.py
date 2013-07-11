605e8dea6f48b9abfe75ccfc577eac06134866e7
Simon de Haan <simon@praekeltfoundation.org>
2013-07-11 21:21:39 +0200

    expose journeys and allow filtering on language

--- system/api.py
+++ system/api.py
@@ -1,5 +1,10 @@
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.resources import ModelResource
+from tastypie import fields
+
 from system.models import Language
+
+from dbs.models import Journey
 
 
 class LanguageResource(ModelResource):
@@ -7,3 +12,16 @@ class LanguageResource(ModelResource):
     class Meta:
         queryset = Language.objects.all()
         resource_name = 'language'
+        allowed_methods = ['get']
+
+
+class JourneyResource(ModelResource):
+    language = fields.ForeignKey(LanguageResource, 'language')
+
+    class Meta:
+        queryset = Journey.objects.all()
+        resource_name = 'journey'
+        allowed_methods = ['get']
+        filtering = {
+            'language': ('exact',),
+        }
