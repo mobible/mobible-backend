e42fd12d1570a6ef13fa6b1dcb381218d07b11e1
Simon de Haan <simon@praekeltfoundation.org>
2013-07-11 21:03:52 +0200

    versioning api

--- system/urls.py
+++ system/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
 from system.api import LanguageResource
+from tastypie.api import Api
 
-language_resource = LanguageResource()
+
+v1_api = Api(api_name='v1')
+v1_api.register(LanguageResource())
 
 urlpatterns = patterns('',
-    (r'^api/', include(language_resource.urls)),
+    (r'^api/', include(v1_api.urls)),
 )
