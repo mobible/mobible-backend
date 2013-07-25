59b611632bd298f0c183a481e9a0793ae1ead26b
Simon de Haan <simon@praekeltfoundation.org>
2013-07-25 21:45:01 +0200

    adding introductions to api

--- system/api.py
+++ system/api.py
@@ -1,7 +1,7 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
 
-from system.models import Language
+from system.models import Language, Introduction
 
 from dbs.models import Journey
 
@@ -22,5 +22,17 @@ class JourneyResource(ModelResource):
         resource_name = 'journey'
         allowed_methods = ['get']
         filtering = {
+            'language': ('exact',),
+        }
+
+
+class IntroductionResource(ModelResource):
+    language = fields.ForeignKey(LanguageResource, 'language', full=True)
+
+    class Meta:
+        queryset = Introduction.objects.all()
+        resource_name = 'introduction'
+        allowed_methods = ['get']
+        filter = {
             'language': ('exact',),
         }
--- system/urls.py
+++ system/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import *
-from system.api import LanguageResource, JourneyResource
+from system.api import LanguageResource, JourneyResource, IntroductionResource
 from tastypie.api import Api
 
 
 v1_api = Api(api_name='v1')
 v1_api.register(LanguageResource())
 v1_api.register(JourneyResource())
+v1_api.register(IntroductionResource())
 
 urlpatterns = patterns('',
     (r'^api/', include(v1_api.urls)),
